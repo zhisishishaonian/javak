5288a58619e3b02b639870143cc1e25dd4ff7c70
lukai <2251584713@qq.com>
2022-04-09 17:50:10 +0800

    我的第一个java程序

---
+++ Java-foundational/src/main/java/net/lukai/java/foundational/object/HolleWorld.java
@@ -0,0 +1,11 @@
+package net.lukai.java.foundational.object;
+
+/**我的第一个java程序
+ * @version 2022/4/9 17:45
+ */
+public class HolleWorld {
+    public static void main(String[] args) {
+        System.out.println("holleworld with windows10 & jdk 18 $git $maven &idea 2022.3.3");
+    }
+
+}
